6c41d8d3756bbc9c158de90e6d08989f45ac5e5a
unknown <stud2009@yandex.ru>
2023-12-11 21:42:23 +0300

    feature: setup tinkoff api search

--- src/main/java/pab/ta/signal/SearchViewController.java
+++ src/main/java/pab/ta/signal/SearchViewController.java
@@ -1,5 +1,6 @@
 package pab.ta.signal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,16 +8,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.tinkoff.piapi.contract.v1.InstrumentShort;
+import ru.tinkoff.piapi.core.InvestApi;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Controller
 @RequestMapping(consumes = MediaType.ALL_VALUE)
 public class SearchViewController {
+
+    private InvestApi investApi;
+
+    @Autowired
+    public void setInvestApi( InvestApi investApi) {
+        this.investApi = investApi;
+    }
+
     @GetMapping(value = "/")
-    public String index(@RequestParam(name = "search", required = false, defaultValue = "") String value, Model model){
+    public String index(@RequestParam(name = "search", required = false, defaultValue = "") String value, Model model) throws ExecutionException, InterruptedException {
 
         if (!value.isEmpty()){
             model.addAttribute("search", value);
-            model.addAttribute("data", new String[]{value, "Apple", "TESLA", "LIVENT"});
+
+            CompletableFuture<List<InstrumentShort>> feature = investApi.getInstrumentsService().findInstrument(value);
+            List<InstrumentShort> instruments = feature.get();
+
+            model.addAttribute("data", instruments);
         }
 
         return "search";
